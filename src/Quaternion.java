539934f42a0ace5cb317f8b753bec42ea16308c7
Jaanus Poeial <jpoial@itcollege.ee>
2022-08-18 13:43:34 +0300

    initial setup

---
+++ src/Quaternion.java
@@ -0,0 +1,200 @@
+import java.util.*;
+
+/** Quaternions. Basic operations. */
+public class Quaternion {
+
+   // TODO!!! Your fields here!
+
+   /** Constructor from four double values.
+    * @param a real part
+    * @param b imaginary part i
+    * @param c imaginary part j
+    * @param d imaginary part k
+    */
+   public Quaternion (double a, double b, double c, double d) {
+      // TODO!!! Your constructor here!
+   }
+
+   /** Real part of the quaternion.
+    * @return real part
+    */
+   public double getRpart() {
+      return 0.; // TODO!!!
+   }
+
+   /** Imaginary part i of the quaternion. 
+    * @return imaginary part i
+    */
+   public double getIpart() {
+      return 0.; // TODO!!!
+   }
+
+   /** Imaginary part j of the quaternion. 
+    * @return imaginary part j
+    */
+   public double getJpart() {
+      return 0.; // TODO!!!
+   }
+
+   /** Imaginary part k of the quaternion. 
+    * @return imaginary part k
+    */
+   public double getKpart() {
+      return 0.; // TODO!!!
+   }
+
+   /** Conversion of the quaternion to the string.
+    * @return a string form of this quaternion: 
+    * "a+bi+cj+dk"
+    * (without any brackets)
+    */
+   @Override
+   public String toString() {
+      return ""; // TODO!!!
+   }
+
+   /** Conversion from the string to the quaternion. 
+    * Reverse to <code>toString</code> method.
+    * @throws IllegalArgumentException if string s does not represent 
+    *     a quaternion (defined by the <code>toString</code> method)
+    * @param s string of form produced by the <code>toString</code> method
+    * @return a quaternion represented by string s
+    */
+   public static Quaternion valueOf (String s) {
+      return null; // TODO!!!
+   }
+
+   /** Clone of the quaternion.
+    * @return independent clone of <code>this</code>
+    */
+   @Override
+   public Object clone() throws CloneNotSupportedException {
+      return null; // TODO!!!
+   }
+
+   /** Test whether the quaternion is zero. 
+    * @return true, if the real part and all the imaginary parts are (close to) zero
+    */
+   public boolean isZero() {
+      return false; // TODO!!!
+   }
+
+   /** Conjugate of the quaternion. Expressed by the formula 
+    *     conjugate(a+bi+cj+dk) = a-bi-cj-dk
+    * @return conjugate of <code>this</code>
+    */
+   public Quaternion conjugate() {
+      return null; // TODO!!! 
+   }
+
+   /** Opposite of the quaternion. Expressed by the formula 
+    *    opposite(a+bi+cj+dk) = -a-bi-cj-dk
+    * @return quaternion <code>-this</code>
+    */
+   public Quaternion opposite() {
+      return null; // TODO!!!
+   }
+
+   /** Sum of quaternions. Expressed by the formula 
+    *    (a1+b1i+c1j+d1k) + (a2+b2i+c2j+d2k) = (a1+a2) + (b1+b2)i + (c1+c2)j + (d1+d2)k
+    * @param q addend
+    * @return quaternion <code>this+q</code>
+    */
+   public Quaternion plus (Quaternion q) {
+      return null; // TODO!!!
+   }
+
+   /** Product of quaternions. Expressed by the formula
+    *  (a1+b1i+c1j+d1k) * (a2+b2i+c2j+d2k) = (a1a2-b1b2-c1c2-d1d2) + (a1b2+b1a2+c1d2-d1c2)i +
+    *  (a1c2-b1d2+c1a2+d1b2)j + (a1d2+b1c2-c1b2+d1a2)k
+    * @param q factor
+    * @return quaternion <code>this*q</code>
+    */
+   public Quaternion times (Quaternion q) {
+      return null; // TODO!!!
+   }
+
+   /** Multiplication by a coefficient.
+    * @param r coefficient
+    * @return quaternion <code>this*r</code>
+    */
+   public Quaternion times (double r) {
+      return null; // TODO!!!
+   }
+
+   /** Inverse of the quaternion. Expressed by the formula
+    *     1/(a+bi+cj+dk) = a/(a*a+b*b+c*c+d*d) + 
+    *     ((-b)/(a*a+b*b+c*c+d*d))i + ((-c)/(a*a+b*b+c*c+d*d))j + ((-d)/(a*a+b*b+c*c+d*d))k
+    * @return quaternion <code>1/this</code>
+    */
+   public Quaternion inverse() {
+      return null; // TODO!!!
+   }
+
+   /** Difference of quaternions. Expressed as addition to the opposite.
+    * @param q subtrahend
+    * @return quaternion <code>this-q</code>
+    */
+   public Quaternion minus (Quaternion q) {
+      return null; // TODO!!!
+   }
+
+   /** Right quotient of quaternions. Expressed as multiplication to the inverse.
+    * @param q (right) divisor
+    * @return quaternion <code>this*inverse(q)</code>
+    */
+   public Quaternion divideByRight (Quaternion q) {
+      return null; // TODO!!!
+   }
+
+   /** Left quotient of quaternions.
+    * @param q (left) divisor
+    * @return quaternion <code>inverse(q)*this</code>
+    */
+   public Quaternion divideByLeft (Quaternion q) {
+      return null; // TODO!!!
+   }
+   
+   /** Equality test of quaternions. Difference of equal numbers
+    *     is (close to) zero.
+    * @param qo second quaternion
+    * @return logical value of the expression <code>this.equals(qo)</code>
+    */
+   @Override
+   public boolean equals (Object qo) {
+      return false; // TODO!!!
+   }
+
+   /** Dot product of quaternions. (p*conjugate(q) + q*conjugate(p))/2
+    * @param q factor
+    * @return dot product of this and q
+    */
+   public Quaternion dotMult (Quaternion q) {
+      return null; // TODO!!!
+   }
+
+   /** Integer hashCode has to be the same for equal objects.
+    * @return hashcode
+    */
+   @Override
+   public int hashCode() {
+      return 0; // TODO!!!
+   }
+
+   /** Norm of the quaternion. Expressed by the formula 
+    *     norm(a+bi+cj+dk) = Math.sqrt(a*a+b*b+c*c+d*d)
+    * @return norm of <code>this</code> (norm is a real number)
+    */
+   public double norm() {
+      return 0.; // TODO!!!
+   }
+
+   /** Main method for testing purposes. 
+    * @param arg command line parameters
+    */
+   public static void main (String[] args) {
+      // TODO!!! Your example runs here
+   }
+
+}
+
